6e7a63977ae75d02eccb0ce7ba6b67c441b004e6
MarianNecula <32199942+MarianNecula@users.noreply.github.com>
2025-04-11 14:36:44 +0300

    Update IC_Lab7.R

--- IC_Lab7.R
+++ IC_Lab7.R
@@ -72,33 +72,6 @@ dim(nci.data)
 nci.labs[1:4]
 table(nci.labs)
 
-### PCA on the NCI60 Data
-
-###
-pr.out <- prcomp(nci.data, scale = TRUE)
-###
-Cols <- function(vec) {
-   cols <- rainbow(length(unique(vec)))
-   return(cols[as.numeric(as.factor(vec))])
- }
-###
-par(mfrow = c(1, 2))
-plot(pr.out$x[, 1:2], col = Cols(nci.labs), pch = 19,
-    xlab = "Z1", ylab = "Z2")
-plot(pr.out$x[, c(1, 3)], col = Cols(nci.labs), pch = 19,
-    xlab = "Z1", ylab = "Z3")
-###
-summary(pr.out)
-###
-plot(pr.out)
-###
-pve <- 100 * pr.out$sdev^2 / sum(pr.out$sdev^2)
-par(mfrow = c(1, 2))
-plot(pve,  type = "o", ylab = "PVE",
-    xlab = "Principal Component", col = "blue")
-plot(cumsum(pve), type = "o", ylab = "Cumulative PVE",
-    xlab = "Principal Component", col = "brown3")
-
 ### Clustering the Observations of the NCI60 Data
 
 ###
